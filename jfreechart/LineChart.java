6045099d1bacd591c4148d98472ce9611f7d53c5
LucasHartman <ljh.hartman@gmail.com>
2022-12-20 18:56:49 +0100

    start jfreechart

---
+++ jfreechart/LineChart.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+import org.jf
+
+public class LineChart extends JFrame {
+    private static final long serialVersionUID=1L;
+
+    public LineChart(String title) {
+        super(title);
+
+        // Create dataset
+        
+    }
+}
